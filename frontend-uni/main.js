cc3d5d03baae3a70853b823a7c47715a6c53d956
Murasame777 <2066220962@qq.com>
2026-07-03 23:26:17 +0800

    Add files via upload

--- frontend-uni/main.js
+++ frontend-uni/main.js
@@ -1,4 +1,10 @@
-import { createSSRApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-export function createApp() { const app = createSSRApp(App); app.use(createPinia()); return { app } }
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+App.mpType = 'app'
+
+const app = new Vue({
+  ...App
+})
+app.$mount()
